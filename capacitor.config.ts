f03228a1d950534979c6301e540370888bb12025
langbart <lorenzo.longobardi@gmail.com>
2025-12-03 23:26:04 +0100

    Update dependencies for Capacitor and related packages
    - Added @capacitor/android, @capacitor/core, and @capacitor/cli with version 7.4.4 to package.json and package-lock.json.
    - Updated the project to ensure compatibility with the latest Capacitor features and improvements.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'worldfish.peskas.tracks',
+  appName: 'PESKAS|tracks',
+  webDir: 'dist'
+};
+
+export default config;
